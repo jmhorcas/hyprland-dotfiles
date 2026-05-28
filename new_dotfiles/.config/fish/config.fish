af60af7502da0dd006aee2003e800b6dcf8c377d
José Miguel Horcas <horcas@uma.es>
2026-05-29 00:51:10 +0200

    new dotfiles

---
+++ new_dotfiles/.config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+export MOZ_ENABLE_WAYLAND=1
+zoxide init fish | source
